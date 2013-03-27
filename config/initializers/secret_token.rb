deb7aefeaef4312e82f3ef165671d9a223b0da5d
baraabourghli <baraa@mindvalley.com>
2013-03-28 00:31:10 +0800

    initial commit, rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timeflow::Application.config.secret_token = '00d90a674ee0245535f075b02b2c87c1ca81b58bfc0667d34d44582c3f288c622b112b7899c72f36dac5cb2224cef833999730a347536d2ac67b6c930e2eeb60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Timeflow::Application.config.session_store :cookie_store, key: '_timeflow_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Timeflow::Application.config.session_store :active_record_store
