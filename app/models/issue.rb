9a322c318f27fae1672bcfe0dfbbc760258e1a3d
baraabourghli <baraa.bourghli@gmail.com>
2013-04-21 16:48:27 +0800

    fix some models, add some and do some modifications

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,9 +3,11 @@ class Issue
 
   embeds_many :comments
   belongs_to :project
-  belongs_to :requester
+  belongs_to :requester # a user can request an issue
+  belongs_to :solver    # a user can solve an issue (can be null at first)
   field :title, type: String
   field :rank, type: String
-  field :type
-  field :priority, type: Integer
+  field :type # issue type : Bug, Feature ... etc
+  field :priority, type: Integer 
+  field :state # the state of the current issue: Not_Started, In_Progress  ... etc
 end
